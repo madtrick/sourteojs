6ef05ef433416114e194ea21bace051f2e4cf336
Farruco Sanjurjo <madtrick@gmail.com>
2009-12-21 21:16:48 +0100

    Initial Javascript code
    *Created skeleton of Sourteo class

---
+++ sourteo.js
@@ -0,0 +1,13 @@
+document.observe("dom:loaded", function(){
+	new Sourteo();
+});
+
+var Sourteo	=	Class.create({
+	initialize	: function(){
+		this._initObservers();
+	}
+	,
+	_initObservers : function(){
+		
+	}
+});
